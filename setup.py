9ad8071c7f6c9fa4572cc9f20e0dbaa2c9cae19b
nilopro <danilomenezes@protonmail.com>
2022-12-29 18:17:56 -0300

    rename project

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='TeleAuth',
+    name='teleauth',
     version='1.0.0',
     description='A library for user authentication in Telegram bots',
     packages=find_packages(),
     install_requires=['prettytable'],
     author='nilopro',
     author_email='menezesdev@pm.me',
-    url='https://github.com/nilopro/TeleAuth',
-    download_url='https://github.com/nilopro/TeleAuth/archive/v1.0.0.tar.gz',
+    url='https://github.com/nilopro/teleauth',
+    download_url='https://github.com/nilopro/teleauth/archive/v1.0.0.tar.gz',
     keywords=['telegram', 'authentication', 'bot'],
     classifiers=[
         'Development Status :: 5 - Production/Stable',
